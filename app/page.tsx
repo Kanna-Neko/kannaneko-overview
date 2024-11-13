9010d30d5b1c8dabb497db5b33d6114b29dd0409
Kanna-Neko <moonandxinxin@gmail.com>
2024-11-13 16:41:03 +0800

    feat: finish all

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,73 @@
+"use client";
+import { FloatingDock } from "@/components/dock";
+import { IoLogoGithub } from "react-icons/io";
 import Image from "next/image";
-
+import { RiZzzFill } from "react-icons/ri";
+import { motion } from "framer-motion";
+import { HiOutlineMail } from "react-icons/hi";
+import { SiCodeforces } from "react-icons/si";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className="bg-[url('/light.png')] dark:bg-[url('/dark.png')] h-screen bg-cover min-w-[320px] relative bg-fixed p-2">
+      <motion.div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+        <Main />
+      </motion.div>
     </div>
+  );
+}
+
+function Main() {
+  const flapDuration = 0.6;
+  const links = [
+    {
+      title: "GitHub",
+      icon: (
+        <IoLogoGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "https://github.com/Kanna-Neko",
+    },
+    {
+      title: "email",
+      icon: (
+        <HiOutlineMail className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "mailto:moonandxinxixn@gmail.com",
+    },
+    {
+      title: "codeforces",
+      icon: (
+        <SiCodeforces className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "https://codeforces.com/profile/kannaneko",
+    },
+  ];
+  return (
+    <motion.div
+      className="p-[clamp(0rem,15vw,5rem)] backdrop-blur-md border-2 dark:border-base-200 rounded-3xl flex flex-col gap-24 items-center"
+      initial={{ rotateY: 90 }}
+      animate={{ rotateY: 0 }}
+      exit={{ rotateY: -90 }}
+      transition={{
+        duration: flapDuration,
+      }}
+    >
+      <motion.div className="flex flex-col gap-8 items-center">
+        <motion.div className="relative">
+          <Image
+            src={"/avatar.png"}
+            height={160}
+            width={160}
+            quality={100}
+            alt="avatar"
+            className="w-[calc(12rem+4vw)] mask mask-circle object-cover"
+          />
+          <RiZzzFill className="absolute bg-base-200 mask mask-circle box-content p-2 right-3 bottom-3 size-[clamp(0.3rem,0.3rem+1.2vw,2rem)]" />
+        </motion.div>
+        <motion.p className="text-4xl self-start text-[clamp(1rem,1rem+3vw,2.25rem)]">
+          Kannaneko
+        </motion.p>
+      </motion.div>
+      <FloatingDock items={links} />
+    </motion.div>
   );
 }
